667a3094d523e1d2841e33ba6ec82df37320b1ec
bec <benoit.chiquet@gmail.com>
2017-01-21 17:11:04 +0100

    Events description

--- Infra/Events.py
+++ Infra/Events.py
@@ -1,9 +1,16 @@
 class Events:
+    """
+    A list of events, with support for reactions.
+    """
     def __init__(self, reactor):
         self.reactor = reactor
         self.events = []
 
     def happening(self, event):
+        """
+        Add an event to the event list, and triggers the reactions for this event.
+        If no reactor is provided, only adds events.
+        """
         self.events.append(event)
         self.reactor.apply_events_triggered_by(event)
         return self
